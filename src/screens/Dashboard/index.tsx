99eba46c30c2fa70024e2482f99d9179ed9ac724
Vinicius Petrachin <vr.petrachin@gmail.com>
2022-02-04 18:04:55 -0300

    - Added Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import { Container } from './styles'
+
+const Dashboard: React.FC = () => {
+	return (
+		<Container>
+			<Text>Teste</Text>
+		</Container>
+	)
+}
+
+export default Dashboard
